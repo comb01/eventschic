2dd52d74beb2ef69101c6df78fc37eb9f15a0a89
comb01 <comb01@hotmail.com>
2019-02-07 00:54:58 -0500

    primer commit de mi sitio web

---
+++ sendemail.php
@@ -0,0 +1,21 @@
+<?php
+	header('Content-type: application/json');
+	$status = array(
+		'type'=>'success',
+		'message'=>'Gracias por ponerse en contacto con nosotros. Tan pronto como sea posible nos pondremos en contacto con usted '
+	);
+
+    $name = @trim(stripslashes($_POST['name'])); 
+    $email = @trim(stripslashes($_POST['email'])); 
+    $subject = @trim(stripslashes($_POST['subject'])); 
+    $message = @trim(stripslashes($_POST['message'])); 
+
+    $email_from = $email;
+    $email_to = 'comb01@hotmail.com';//replace with your email
+
+    $body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
+
+    $success = @mail($email_to, $subject, $body, 'From: <'.$email_from.'>');
+
+    echo json_encode($status);
+    die;
